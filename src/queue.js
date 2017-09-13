a980c1d22aadef753bd2dd18829c32096b5ff055
douglasxtaylor <douglasxtaylor@icloud.com>
2017-09-13 12:50:13 -0700

    working except for hash-table deletion

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,16 @@
 class Queue {
-
+  constructor() {
+    this.waitList = [];
+  }
+  enqueue(patient) {
+    this.waitList.push(patient);
+  }
+  dequeue() {
+    return this.waitList.shift();
+  }
+  get size() {
+    return this.waitList.length;
+  }
 }
 
 module.exports = Queue;
--- src/stack.js
+++ src/stack.js
@@ -1,5 +1,16 @@
-class Stack {
-
+class Stack { // arranged this stack into something like an email subscription list, although the data would probably not be stored this way.
+  constructor() {
+    this.storage = [];// push or pop from here
+  }
+  push(node) { // data entry into the node
+    this.storage.push(node);
+  }
+  pop() { // pops the last one off. LIFO
+    return this.storage.pop();
+  }
+  get size() { // we have getters and setters in ES6. Size is a getter.
+    return this.storage.length;
+  }
 }
-
+const subscriptionList = new Stack();
 module.exports = Stack;
